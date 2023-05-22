7578305a4017322e4c3036ee980eccedc2ec34f5
dkalabukhov <daniil.kalabukhov@gmail.com>
2023-05-22 21:15:43 +0300

    Step 1 done

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import _ from 'lodash';
 
 export default function solution(content){
-  // BEGIN
-  console.log(content);
-  // END
+
+  // Data Normalization
+  const rows = content.split('\r\n');
+  const data = rows.slice(1, rows.length - 1);
+
+  const chessGames = data.map((game) => game.split(','));
+
+  const normalizedData = chessGames.map((game) => game.filter((element) => element));
+
+  // Step 1
+  console.log(`Количество партий: ${normalizedData.length}`);
+
 }
